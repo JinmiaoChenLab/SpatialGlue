0bf85a54dc87869a0ae0b4382c1723959ba8a869
longyahui <60100581+longyahui@users.noreply.github.com>
2023-12-13 17:05:33 +0800

    Update preprocess.py

--- SpatialGlue/preprocess.py
+++ SpatialGlue/preprocess.py
@@ -13,81 +13,69 @@ from torch.backends import cudnn
 from scipy.sparse import coo_matrix
 from sklearn.neighbors import NearestNeighbors
 from sklearn.neighbors import kneighbors_graph 
-
-def load_data():
-    """
-    Reading adata
-    """
-    # read adata
-    ## mouse spleen (SPOTS)
-    adata_omics1 = sc.read_h5ad('/home/yahui/anaconda3/work/SpatialGlue_omics/data/SPOTS/Mouse_Spleen/Replicate1/adata_RNA.h5ad')
-    adata_omics2 = sc.read_h5ad('/home/yahui/anaconda3/work/SpatialGlue_omics/data/SPOTS/Mouse_Spleen/Replicate1/adata_Pro.h5ad')  
     
-    ## mouse thymus (Stereo-CITE-seq)
-    #adata_omics1 = sc.read_h5ad('/home/yahui/anaconda3/work/SpatialGlue_omics/data/Thymus/adata_RNA.h5ad')
-    #adata_omics2 = sc.read_h5ad('/home/yahui/anaconda3/work/SpatialGlue_omics/data/Thymus/adata_ADT.h5ad')
+def preprocessing(adata_omics1, adata_omics2, datatype='SPOTS', n_neighbors=3): 
     
-    ## mouse brain (Spatial-ATAC-RNA-seq)
-    #adata_omics1 = sc.read_h5ad('/home/yahui/anaconda3/work/SpatialGlue_omics/data/Rong_Fan/Mouse_Brain/Mouse_Brain_P22/Mouse_Brain_P22/adata_RNA.h5ad')
-    #adata_omics2 = sc.read_h5ad('/home/yahui/anaconda3/work/SpatialGlue_omics/data/Rong_Fan/Mouse_Brain/Mouse_Brain_P22/Mouse_Brain_P22/adata_peaks_normalized.h5ad')
+    # configure random seed
+    random_seed=2022
+    fix_seed(random_seed)
     
-    # make feature names unique
-    adata_omics1.var_names_make_unique()
-    adata_omics2.var_names_make_unique()
-
-    return adata_omics1, adata_omics2
-    
-def preprocessing(adata_omics1, adata_omics2, datatype='SPOTS', n_neighbors=6): 
-    
-    if datatype not in ['SPOTS', 'Stereo-CITE-seq', 'Spatial-ATAC-RNA-seq']:
+    if datatype not in ['10x', 'SPOTS', 'Stereo-CITE-seq', 'Spatial-epigenome-transcriptome']:
       raise ValueError("The datatype is not supported now. SpatialGlue supports 'SPOTS', 'Stereo-CITE-seq', 'Spatial-ATAC-RNA-seq'. We would extend SpatialGlue for more data types. ") 
     
-    if datatype == 'SPOTS':  # mouse spleen 
+    if datatype == 'SPOTS':   
       # RNA
       sc.pp.filter_genes(adata_omics1, min_cells=10)
       sc.pp.highly_variable_genes(adata_omics1, flavor="seurat_v3", n_top_genes=3000)
       sc.pp.normalize_total(adata_omics1, target_sum=1e4)
       sc.pp.log1p(adata_omics1)
-      #sc.pp.pca(adata_omics1, use_highly_variable=True, n_comps=20) #22
-      #adata_omics1.obsm['feat'] = adata_omics1.obsm['X_pca'].copy()
+      sc.pp.scale(adata_omics1)
       
       adata_omics1_high =  adata_omics1[:, adata_omics1.var['highly_variable']]
-      sc.pp.pca(adata_omics1_high, n_comps=20)  #30
-      adata_omics1.obsm['feat'] = adata_omics1_high.obsm['X_pca']
+      adata_omics1.obsm['feat'] = pca(adata_omics1_high, n_comps=adata_omics2.n_vars-1)
     
       # Protein
       adata_omics2 = clr_normalize_each_cell(adata_omics2)
-      sc.pp.pca(adata_omics2, n_comps=20)
-      adata_omics2.obsm['feat'] = adata_omics2.obsm['X_pca'].copy()
+      sc.pp.scale(adata_omics2)
+      adata_omics2.obsm['feat'] = pca(adata_omics2, n_comps=adata_omics2.n_vars-1)
+     
       
     elif datatype == 'Stereo-CITE-seq':  
       # RNA
       sc.pp.filter_genes(adata_omics1, min_cells=10)
       sc.pp.filter_cells(adata_omics1, min_genes=80)
+      
+      sc.pp.filter_genes(adata_omics2, min_cells=50)
+      adata_omics2 = adata_omics2[adata_omics1.obs_names].copy()
 
       sc.pp.highly_variable_genes(adata_omics1, flavor="seurat_v3", n_top_genes=3000)
       sc.pp.normalize_total(adata_omics1, target_sum=1e4)
       sc.pp.log1p(adata_omics1)
-      #sc.pp.pca(adata_omics1, use_highly_variable=True, n_comps=50)
-      #adata_omics1.obsm['feat'] = adata_omics1.obsm['X_pca'].copy()
       
       adata_omics1_high =  adata_omics1[:, adata_omics1.var['highly_variable']]
-      sc.pp.pca(adata_omics1_high, n_comps=18)  #18
-      adata_omics1.obsm['feat'] = adata_omics1_high.obsm['X_pca']
+      adata_omics1.obsm['feat'] = pca(adata_omics1_high, n_comps=adata_omics2.n_vars-1)
       
       # Protein
-      sc.pp.filter_genes(adata_omics2, min_cells=50)
-      proteins_to_remove = ['Mouse-IgD','Mouse-CD140a']
-      proteins_to_keep = ~adata_omics2.var_names.isin(proteins_to_remove)
-      adata_omics2 = adata_omics2[:, proteins_to_keep]
-      
-      adata_omics2 = adata_omics2[adata_omics1.obs_names].copy()
-      
       adata_omics2 = clr_normalize_each_cell(adata_omics2)
-      sc.pp.pca(adata_omics2, n_comps=18) #18
-      adata_omics2.obsm['feat'] = adata_omics2.obsm['X_pca'].copy()
+      adata_omics2.obsm['feat'] = pca(adata_omics2, n_comps=adata_omics2.n_vars-1)
       
-    elif datatype == 'Spatial-ATAC-RNA-seq':  
+    elif datatype == '10x':  
+      # RNA
+      sc.pp.filter_genes(adata_omics1, min_cells=10)
+      sc.pp.highly_variable_genes(adata_omics1, flavor="seurat_v3", n_top_genes=3000)
+      sc.pp.normalize_total(adata_omics1, target_sum=1e4)
+      sc.pp.log1p(adata_omics1)
+      sc.pp.scale(adata_omics1)
+      
+      adata_omics1_high =  adata_omics1[:, adata_omics1.var['highly_variable']]
+      adata_omics1.obsm['feat'] = pca(adata_omics1_high, n_comps=adata_omics2.n_vars-1)
+    
+      # Protein
+      adata_omics2 = clr_normalize_each_cell(adata_omics2)
+      sc.pp.scale(adata_omics2)
+      adata_omics2.obsm['feat'] = pca(adata_omics2, n_comps=adata_omics2.n_vars-1)  
+      
+    elif datatype == 'Spatial-epigenome-transcriptome':  
       # RNA
       sc.pp.filter_genes(adata_omics1, min_cells=10)
       sc.pp.filter_cells(adata_omics1, min_genes=200)
@@ -95,27 +83,29 @@ def preprocessing(adata_omics1, adata_omics2, datatype='SPOTS', n_neighbors=6):
       sc.pp.highly_variable_genes(adata_omics1, flavor="seurat_v3", n_top_genes=3000)
       sc.pp.normalize_total(adata_omics1, target_sum=1e4)
       sc.pp.log1p(adata_omics1)
-      #sc.pp.pca(adata_omics1, use_highly_variable=True, n_comps=50)
-      #adata_omics1.obsm['feat'] = adata_omics1.obsm['X_pca'].copy()
+      sc.pp.scale(adata_omics1)
       
       adata_omics1_high =  adata_omics1[:, adata_omics1.var['highly_variable']]
-      sc.pp.pca(adata_omics1_high, n_comps=50)  #30
-      adata_omics1.obsm['feat'] = adata_omics1_high.obsm['X_pca']
+      adata_omics1.obsm['feat'] = pca(adata_omics1_high, n_comps=50)
       
       # ATAC
       adata_omics2 = adata_omics2[adata_omics1.obs_names].copy() # .obsm['X_lsi'] represents the dimension reduced feature
+      if 'X_lsi' not in adata_omics2.obsm.keys():
+          sc.pp.highly_variable_genes(adata_omics2, flavor="seurat_v3", n_top_genes=3000)
+          lsi(adata_omics2, use_highly_variable=False, n_components=51)
+          
       adata_omics2.obsm['feat'] = adata_omics2.obsm['X_lsi'].copy()
 
-    # construct cell neighbor graphs
+    # construct spatial neighbor graphs
     ################# spatial graph #################
     # omics1
     cell_position_omics1 = adata_omics1.obsm['spatial']
-    adj_omics1 = build_network(cell_position_omics1, n_neighbors=6)
+    adj_omics1 = build_network(cell_position_omics1, n_neighbors=3)
     adata_omics1.uns['adj_spatial'] = adj_omics1
     
     # omics2
     cell_position_omics2 = adata_omics2.obsm['spatial']
-    adj_omics2 = build_network(cell_position_omics2, n_neighbors=6)
+    adj_omics2 = build_network(cell_position_omics2, n_neighbors=3)
     adata_omics2.uns['adj_spatial'] = adj_omics2
     
     ################# feature graph #################
@@ -126,7 +116,26 @@ def preprocessing(adata_omics1, adata_omics2, datatype='SPOTS', n_neighbors=6):
     
     return data
 
+def pca(adata, use_reps=None, n_comps=10):
+    
+    """Dimension reduction with PCA algorithm"""
+    
+    from sklearn.decomposition import PCA
+    from scipy.sparse.csc import csc_matrix
+    from scipy.sparse.csr import csr_matrix
+    pca = PCA(n_components=n_comps)
+    if use_reps is not None:
+       feat_pca = pca.fit_transform(adata.obsm[use_reps])
+    else: 
+       if isinstance(adata.X, csc_matrix) or isinstance(adata.X, csr_matrix):
+          feat_pca = pca.fit_transform(adata.X.toarray()) 
+       else:   
+          feat_pca = pca.fit_transform(adata.X)
+    
+    return feat_pca
+
 def clr_normalize_each_cell(adata, inplace=True):
+    
     """Normalize count vector for each cell, i.e. for each row of .X"""
 
     import numpy as np
@@ -156,7 +165,7 @@ def construct_graph_by_feature(adata_omics1, adata_omics2, k=20, mode= "connecti
 
     return feature_graph_omics1, feature_graph_omics2
 
-def build_network(cell_position, n_neighbors=6):
+def build_network(cell_position, n_neighbors=3):
     
     """Constructing spatial neighbor graph according to spatial coordinates."""
     
