2a5b9ca8eea102fde62973de8ac791a2e0ddd545
longyahui <60100581+longyahui@users.noreply.github.com>
2023-12-14 10:51:43 +0800

    Update utils.py

--- SpatialGlue/utils.py
+++ SpatialGlue/utils.py
@@ -63,7 +63,7 @@ def clustering(adata, n_clusters=7, key='emb', add_key='SpatialGlue', method='mc
     """
     
     if use_pca:
-       adata.obsm[key + '_pca'] = pca(adata, n_comps=n_comps)
+       adata.obsm[key + '_pca'] = pca(adata, use_reps=key, n_comps=n_comps)
     
     if method == 'mclust':
        if use_pca: 
